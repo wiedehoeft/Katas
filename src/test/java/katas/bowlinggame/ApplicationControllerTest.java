06b3bf07a21b4437bc8a45649215b0b753663e24
Christian <wiedehoeft.christian@gmail.com>
2017-12-19 13:12:40 +0100

    Clean up

---
+++ src/test/java/katas/bowlinggame/ApplicationControllerTest.java
@@ -0,0 +1,58 @@
+package katas.bowlinggame;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class DialogControllerTest {
+
+  private Application application;
+
+  @Mock
+  private DialogController dialogController;
+
+  @Mock
+  private IOController ioController;
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+    application = new Application(dialogController, ioController);
+  }
+
+  @Test
+  public void testThatUserHasToEnterNameAfterApplicationStarts() throws Exception {
+
+    // When
+    application.start();
+
+    // Then
+    Mockito.verify(dialogController).printGreetingAndGetUserName();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testThatUserHasToChooseAnOptionAfterMenuShows() throws Exception {
+    // Given
+    Mockito.when(ioController.getUserInput()).thenReturn(" ");
+
+    //When
+    application.showOptionMenu();
+
+    //Then
+    Mockito.verify(dialogController).showOptionMenu();
+  }
+
+  @Test
+  public void testUserChoosesValidOption() throws Exception {
+    // Given
+    Mockito.when(ioController.getUserInput()).thenReturn("1");
+
+    //When
+    application.showOptionMenu();
+
+    //Then
+    Mockito.verify(dialogController).startGame();
+  }
+}
